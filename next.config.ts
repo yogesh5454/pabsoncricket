2da14768dbd78253b6b831a5d6fa08800ee69807
Ashimpaudel09 <ashimpa32@gmail.com>
2026-01-26 18:56:43 +0545

    commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  env: {
+    SITE_TITLE: "Pabson Cricket League",
+    SITE_DESCRIPTION: "Pabson Cricket League is a premier school-level cricket tournament showcasing the best young cricketing talent. Follow live scores, match schedules, player stats, and highlights of the thrilling competition, all in one platform.",
+    SITE_URL: "https://pabsoncricketleague.com", // Replace with your live URL
+    SITE_IMAGE: "https://res.cloudinary.com/dmlntyta1/image/upload/v1769432426/pablogo_sreyip.png" // Replace with your OG image
+  },
+  images: {
+    domains: ['pabsoncricketleague.com'], // Replace with domains for external images
+  }
 };
 
 export default nextConfig;
